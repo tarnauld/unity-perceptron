a8f9c53e44db0612d1de6929c6a50d034274dbd1
timothee.arnauld1@gmail.com <timothee.arnauld1@gmail.com>
2018-03-06 16:34:34 +0100

    Initial commit

---
+++ Scripts/InitScene.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class InitScene : MonoBehaviour
+{
+	private List<GameObject> _gameObjects = new List<GameObject>();
+
+	[SerializeField]
+	private GameObject sphereBlue;
+	
+	[SerializeField]
+	private GameObject sphereRed;
+	
+	[SerializeField]
+	private GameObject sphereWhite;
+	
+	// Use this for initialization
+	void Start () {
+
+		for (var i = -10; i < 10; i++)
+		{
+			for (var j = -10; j < 10; j++)
+			{
+				Instantiate(sphereWhite, new Vector3(i, j), Quaternion.identity);
+			}	
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
