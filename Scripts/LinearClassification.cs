c3fec98eb5b4e3c9176957065256b4d12b2baae2
Aurelien Duval <aurelien.duval6@gmail.com>
2018-03-08 10:27:24 +0100

    Corrected linear classification

--- Scripts/LinearClassification.cs
+++ Scripts/LinearClassification.cs
@@ -11,7 +11,7 @@ public class LinearClassification : MonoBehaviour {
     private static extern System.IntPtr weights_training(System.IntPtr ptr, double[] data_set, long nb_points);
 
     [DllImport("machine_learning")]
-    private static extern double classify(double   [] row, double[] ptr);
+    private static extern double classify(double   [] row, System.IntPtr ptr);
 
     [SerializeField]
     private GameObject[] spheres;
@@ -21,18 +21,6 @@ public class LinearClassification : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-        Debug.Log(classify( new[] {1.0d, 1.0d }, new[] { 1.0d, 1.0d, 1.0d }));
-        Debug.Log(classify(new[] { -1.0d, -1.0d }, new[] { 1.0d, 1.0d, 1.0d }));
-        Debug.Log(classify(new[] { 1.0d, -1.0d }, new[] { 1.0d, 1.0d, 1.0d }));
-
-        Debug.Log(classify(new[] { 1.0d, 1.001d }, new[] { 0.0d, -1.0d, 1.0d }));
-        Debug.Log(classify(new[] { 1.0d, 0.999d }, new[] { 0.0d, -1.0d, 1.0d }));
-        Debug.Log(classify(new[] { 0.0d, -1.0d }, new[] { 0.0d, -1.0d, 1.0d }));
-
-        Debug.Log(classify(new[] { 1.0d, 1.0d }, new[] { -0.5d, 1.0d, 0.0d }));
-        Debug.Log(classify(new[] { -1.0d, -1.0d }, new[] { -0.5d, 1.0d, 0.0d }));
-        Debug.Log(classify(new[] { -1.0d, 1.0d }, new[] { -0.5d, 1.0d, 0.0d }));
-
 
         System.IntPtr ptr = generate_weight();
         var tmp = new double[3];
@@ -55,16 +43,16 @@ public class LinearClassification : MonoBehaviour {
         }
         
         //System.IntPtr ptr = generate_weight();
-        ptr = weights_training(ptr, list.ToArray(), list.ToArray().Length);
+        ptr = weights_training(ptr, list.ToArray(), list.ToArray().Length / 3);
         
         for (var i = 0; i < gameObject.Length; i++)
         {
             //var tmp = new double[3];
             Marshal.Copy(ptr, tmp, 0, 3);
             List<double> l = new List<double>();
-            l.Add(gameObject[i].transform.localPosition.x);
-            l.Add(gameObject[i].transform.localPosition.z);
-            if (classify(l.ToArray(), tmp) > 0)
+            l.Add((double)gameObject[i].transform.localPosition.x);
+            l.Add((double)gameObject[i].transform.localPosition.z);
+            if (classify(l.ToArray(), ptr) > 0)
             {
                 gameObject[i].transform.position += Vector3.up;
             }
